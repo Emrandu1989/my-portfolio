a73d701dfe798f7cdf76efcd52da7d976b2201d7
Emrandu1989 <emrandu1989@gmail.com>
2024-07-04 14:52:54 +0600

    some changes in the projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -15,10 +15,40 @@ const Projects = () => {
           <div className="card-body items-center text-center">
           <figure>
     <img
-      src="../../../src/assets/machine-world.png"
+      src="machine-world.png"
       alt="Shoes" />
   </figure>
             <h2 className="card-title text-2xl  font-bold">Machine-World</h2>
+            <div className="mt-4 flex gap-5 justify-center items-center">
+            
+              <p className="text-blue-600">
+                <a
+                  href="https://machine-world.netlify.app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Live Project
+                </a>
+              </p>
+              <p className="text-blue-600">
+                <a
+                  href="https://github.com/Emrandu1989/a12-client"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub Client
+                </a>
+              </p>
+              <p className="text-blue-600">
+                <a
+                  href="https://github.com/Emrandu1989/a12-server"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub Server
+                </a>
+              </p>
+            </div>
             <p className="text-gray-600 mt-4">
               Machine World is an employee management system for tracking tasks,
               wages, and agreements. Employees can post workflow updates, and HR
@@ -49,75 +79,19 @@ const Projects = () => {
                 Stripe, and various npm packages used for functionality.
               </p>
             </div>
-            <div className="mt-4 flex gap-5 justify-center items-center">
-              <h3 className="text-lg font-semibold">Links:</h3>
-              <p className="text-blue-600">
-                <a
-                  href="https://machine-world.netlify.app"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Live Project
-                </a>
-              </p>
-              <p className="text-blue-600">
-                <a
-                  href="https://github.com/Emrandu1989/a12-client"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub Client
-                </a>
-              </p>
-              <p className="text-blue-600">
-                <a
-                  href="https://github.com/Emrandu1989/a12-server"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub Server
-                </a>
-              </p>
-            </div>
+         
           </div>
         </div>
         <div className="card   lg:bg-base-200  shadow-xl lg:w-[800px]">
           <div className="card-body items-center text-center">
           <figure>
     <img
-      src="../../../src/assets/epicurianArena.png"
+      src="epicurianArena.png"
       alt="Shoes" />
   </figure>
             <h2 className="card-title text-2xl font-bold">Epicurean-Arena</h2>
-            <p className="text-gray-600 mt-4">
-            Epicurean Arena is a community platform that reduces food waste and encourages food sharing. Users can easily donate surplus food, request donations, and manage contributions.
-            </p>
-            <div className="mt-4">
-              <h3 className="text-lg font-semibold">Features:</h3>
-              <ul className="list-disc list-inside text-left mt-2 text-gray-700">
-                <li>
-                  Developed a responsive Restaurant website with signup and
-                  Login form.
-                </li>
-                <li>
-                  Implement Add Food, Available Food and Single Food Details
-                  Page
-                </li>
-                <li>
-                  Ensured a seamless user experience through intuitive design
-                </li>
-              </ul>
-            </div>
-            <div className="mt-4">
-              <h3 className="text-lg font-semibold">Technologies:</h3>
-              <p className="text-gray-700">
-                {" "}
-                React.js, Tailwind CSS, DaisyUI, Firebase, Express.js, MongoDB,
-                various npm packages used for functionality.
-              </p>
-            </div>
             <div className="mt-4 flex gap-5 justify-center items-center">
-              <h3 className="text-lg font-semibold">Links:</h3>
+          
               <p className="text-blue-600">
                 <a
                   href="https://epicurian-arena.netlify.app"
@@ -146,16 +120,66 @@ const Projects = () => {
                 </a>
               </p>
             </div>
+            <p className="text-gray-600 mt-4">
+            Epicurean Arena is a community platform that reduces food waste and encourages food sharing. Users can easily donate surplus food, request donations, and manage contributions.
+            </p>
+            <div className="mt-4">
+              <h3 className="text-lg font-semibold">Features:</h3>
+              <ul className="list-disc list-inside text-left mt-2 text-gray-700">
+                <li>
+                  Developed a responsive Restaurant website with signup and
+                  Login form.
+                </li>
+                <li>
+                  Implement Add Food, Available Food and Single Food Details
+                  Page
+                </li>
+                <li>
+                  Ensured a seamless user experience through intuitive design
+                </li>
+              </ul>
+            </div>
+            <div className="mt-4">
+              <h3 className="text-lg font-semibold">Technologies:</h3>
+              <p className="text-gray-700">
+                {" "}
+                React.js, Tailwind CSS, DaisyUI, Firebase, Express.js, MongoDB,
+                various npm packages used for functionality.
+              </p>
+            </div>
+         
           </div>
         </div>
         <div className="card  lg:bg-base-200 shadow-xl w-[1300px]">
           <div className="card-body items-center text-center">
           <figure>
     <img
-      src="../../../src/assets/regalResidence.png"
+      src="regalResidence.png"
       alt="Shoes" />
   </figure>
             <h2 className="card-title text-2xl  font-bold">Regal Residences</h2>
+            <div className="mt-4 flex gap-5 justify-center items-center">
+           
+              <p className="text-blue-600">
+                <a
+                  href="https://regalresidence.netlify.app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Live Project
+                </a>
+              </p>
+              <p className="text-blue-600">
+                <a
+                  href="https://github.com/programming-hero-web-course-4/b9a9-real-estate-Emrandu1989"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub Link
+                </a>
+              </p>
+            
+            </div>
             <p className="text-gray-600 mt-4">
             Epicurean Arena is a community platform that reduces food waste and encourages food sharing. Users can easily donate surplus food, request donations, and manage contributions.
             </p>
@@ -177,31 +201,10 @@ const Projects = () => {
               <h3 className="text-lg font-semibold">Technologies:</h3>
               <p className="text-gray-700">
                 {" "}
-                React, Tailwind CSS, Daisy UI, Firebase, MongoDB, Animista.
+                ReactJS, Tailwind CSS, Daisy UI, Firebase and some npm packages.
               </p>
             </div>
-            <div className="mt-4 flex gap-5 justify-center items-center">
-              <h3 className="text-lg font-semibold">Links:</h3>
-              <p className="text-blue-600">
-                <a
-                  href="https://regalresidence.netlify.app"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Live Project
-                </a>
-              </p>
-              <p className="text-blue-600">
-                <a
-                  href="https://github.com/programming-hero-web-course-4/b9a9-real-estate-Emrandu1989"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub Link
-                </a>
-              </p>
-            
-            </div>
+       
           </div>
         </div>
       </div>
